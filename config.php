0dc2f94088230b650e1c3fd37ba07defb9fced90
Project343 <35072818+rendracoder@users.noreply.github.com>
2019-05-13 16:08:59 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$voyager_api_key = "xxxxxxxxxx";
+$buzzbreak_api_key = "xxxxxxxxxx";
+$buzzbreak_locale = "xxxxxxxxxx";
+$buzzbreak_account_id = xxxxxxxxxx;
+
+$device_model= "xxxxxxxxxx";
+$app_version_code = xxxxxxxxxx;
+$device_type = "xxxxxxxxxx";
+$facebook_user_id = "xxxxxxxxxx";
+$device_id = "xxxxxxxxxx";
+$facebook_access_token = "xxxxxxxxxx";
+$visitor_id = xxxxxxxxxx;
+?>
